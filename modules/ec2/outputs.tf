1186bb9c224b1ceb32991887823fe9604e2c1cb6
rohitj080 <rohitj080@gmail.com>
2026-03-22 15:35:22 +0530

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,1 +1,19 @@
-hi output
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.ec2.id
+}
+
+output "public_ip" {
+  description = "EC2 Public IP"
+  value       = aws_instance.ec2.public_ip
+}
+
+output "private_ip" {
+  description = "EC2 Private IP"
+  value       = aws_instance.ec2.private_ip
+}
+
+output "instance_arn" {
+  description = "EC2 ARN"
+  value       = aws_instance.ec2.arn
+}
