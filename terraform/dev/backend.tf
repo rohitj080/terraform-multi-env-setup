7addb882408af67ad305d169dfcd6d07d2d467d1
rohitj080 <rohitj080@gmail.com>
2026-03-22 17:11:22 +0530

    Update backend.tf

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "test-poc-jordan-tfstate-${var.environment}"
-    key            = "dev/terraform.tfstate"   # change per environment
+    bucket         = "test-poc-jordan-tfstate-${var.environment}"  # unique per env
+    key            = "${var.environment}/terraform.tfstate"        # unique per env
     region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform-lock"                              # same for all envs
     encrypt        = true
   }
 }
