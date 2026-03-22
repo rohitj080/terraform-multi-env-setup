7b4e588bfbfcaae9593d8f89e09f276872c65593
rohitj080 <rohitj080@gmail.com>
2026-03-22 15:32:15 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,20 +1,38 @@
-resource "aws_vpc" "multi_env_vpc" {
-  cidr_block = var.cidr_block
+resource "aws_vpc" "main" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name        = var.name
-    Environment = var.environment
+    Name = var.name
   }
 }
 
-resource "aws_subnet" "public_subnet1" {
-  vpc_id                  = aws_vpc.multi-env-vpc.id
-  cidr_block              = var.public_subnet1_cidr
-  availability_zone       = var.availability_zone1
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = var.az
   map_public_ip_on_launch = true
 
   tags = {
-    Name        = "${var.name}-public-subnet1"
-    Environment = var.environment
+    Name = "${var.name}-subnet"
   }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.rt.id
 }
