0cbd060126814aa9c25479af7a0483478b51066f
rohitj080 <rohitj080@gmail.com>
2026-03-22 15:44:18 +0530

    Update outputs.tf

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,1 +1,11 @@
-hi outputfile
+output "bucket_id" {
+  value = aws_s3_bucket.this.id
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.this.arn
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.this.bucket
+}
