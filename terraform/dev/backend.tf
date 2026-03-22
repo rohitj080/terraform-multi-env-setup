129644f6b9c8da3087afc07420857d6cbd37a38c
rohitj080 <rohitj080@gmail.com>
2026-03-22 17:10:05 +0530

    Update backend.tf

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-1405"
+    bucket         = "test-poc-jordan-tfstate-${var.environment}"
     key            = "dev/terraform.tfstate"   # change per environment
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
