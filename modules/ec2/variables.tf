176bb9d91b83a6ca698b80d8c14a8ed10138617f
rohitj080 <rohitj080@gmail.com>
2026-03-22 15:36:58 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,1 +1,34 @@
-hi var
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where EC2 will be launched"
+  type        = string
+}
+
+variable "sg_id" {
+  description = "Security Group ID for EC2"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key pair name for SSH access"
+  type        = string
+}
+
+variable "name" {
+  description = "Name tag for EC2 instance"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (dev/uat/prod)"
+  type        = string
+}
