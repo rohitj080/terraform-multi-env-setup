2f196d5d486b6c60b459962d14e15643431e3917
rohitj080 <rohitj080@gmail.com>
2026-03-22 11:39:10 +0530

    Update variables.tf

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -1,6 +1,23 @@
-variable "aws_region" {}
-variable "vpc_cidr" {}
-variable "subnet_cidr" {}
-variable "availability_zone1" {}
-variable "ami_id" {}
-variable "instance_type" {}
+variable "aws_region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "subnet_cidr" {
+  type = string
+}
+
+variable "availability_zone1" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
