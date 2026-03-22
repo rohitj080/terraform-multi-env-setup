93d90aafdab4d7ad6ac330117d85f1e9708987fe
rohitj080 <rohitj080@gmail.com>
2026-03-22 15:34:06 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,7 +1,11 @@
 output "subnet_id" {
-  value = aws_subnet.public_subnet1.id
+  value = aws_subnet.public.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.multi-env-vpc.id
+  value = aws_vpc.main.id
+}
+
+output "sg_id" {
+  value = aws_security_group.ec2_sg.id
 }
