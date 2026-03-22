9280b33a87eb98e197e21a05a1fbca24d3feb42e
rohitj080 <rohitj080@gmail.com>
2026-03-22 17:19:28 +0530

    Create variables.tf

---
+++ terraform/prod/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "Public subnet CIDR block"
+  type        = string
+}
+
+variable "availability_zone1" {
+  description = "Availability Zone"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (dev/uat/prod)"
+  type        = string
+  default     = "dev"
+}
