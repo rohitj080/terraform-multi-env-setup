dbe94354b9dbe4e57946dd0c4d2f45b298832459
rohitj080 <rohitj080@gmail.com>
2026-03-22 15:19:39 +0530

    Update backend.tf

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,1 +1,9 @@
-hi backend
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-1405"
+    key            = "dev/terraform.tfstate"   # change per environment
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
