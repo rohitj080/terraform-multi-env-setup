f2edc95656d743e6e519f30a3c8b6b4c90a4e5bc
rohitj080 <rohitj080@gmail.com>
2026-03-22 11:28:36 +0530

    Update main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,3 +1,23 @@
+provider "aws" {
+    region = var.aws_region
+  
+}
+
 module "vpc" {
-  source = "../../modules/vpc"
+    source  = "../../modules/vpc"
+    cidr_block          = var.vpc_cidr
+    subnet_cidr         = var.subnet_cidr
+    availability_zone1  = var.availability_zone1
+    name                = "dev-vpc"
+    public_subnet1_cidr = var.subnet_cidr
+    environment         = "dev"
+}
+
+module "ec2" {
+    source = "../../modules/ec2"     
+    ami_id = var.ami_id
+    instance_type = var.instance_type
+    subnet_id = module.vpc.subnet_id
+    name = "dev-ec2-instance"
+    environment = "dev"
 }
