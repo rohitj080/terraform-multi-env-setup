dfec34f2bd8fea20f60c238908088abba8f48e98
rohitj080 <rohitj080@gmail.com>
2026-03-22 15:32:49 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,12 @@
-hi main
+resource "aws_instance" "ec2" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.sg_id]
+  key_name                    = var.key_name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = var.name
+  }
+}
