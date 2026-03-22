f6e13cde93b12b7dc0cedfb9153b59245ce78c55
rohitj080 <rohitj080@gmail.com>
2026-03-22 11:34:56 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,20 +1,20 @@
 resource "aws_vpc" "multi-env-vpc" {
   cidr_block = var.cidr_block
-  
-  tags       = {
-    Name = "var.name"
+
+  tags = {
+    Name        = var.name
+    Environment = var.environment
   }
-  
 }
 
 resource "aws_subnet" "public_subnet1" {
-  vpc_id            = aws_vpc.multi-env-vpc.id
-  cidr_block        = var.public_subnet1_cidr
-  availability_zone = var.availability_zone1
-  
-  tags              = {
-    Name = "${var.name}-public-subnet1"
+  vpc_id                  = aws_vpc.multi-env-vpc.id
+  cidr_block              = var.public_subnet1_cidr
+  availability_zone       = var.availability_zone1
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.name}-public-subnet1"
     Environment = var.environment
   }
-  
 }
