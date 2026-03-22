cbef0ad64fd110fa6618f824e360b1a3ab08e9c6
rohitj080 <rohitj080@gmail.com>
2026-03-22 11:31:52 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,8 @@
-hi var
+variable "cidr_block" {}
+variable "subnet_cidr" {}
+variable "availability_zone1" {}
+variable "name" {
+    default = "multi-env-github-actions-vpc"
+}
+variable "public_subnet1_cidr" {}
+variable "environment" {}
