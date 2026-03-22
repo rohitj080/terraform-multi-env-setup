cec4112e199489a5562247acd69cd70c9f0133a2
rohitj080 <rohitj080@gmail.com>
2026-03-22 11:31:24 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,20 @@
-hi main
+resource "aws_vpc" "multi-env-vpc" {
+  cidr_block = var.cidr_block
+  
+  tags       = {
+    Name = "var.name"
+  }
+  
+}
+
+resource "aws_subnet" "public_subnet1" {
+  vpc_id            = aws_vpc.multi-env-vpc.id
+  cidr_block        = var.public_subnet1_cidr
+  availability_zone = var.availability_zone1
+  
+  tags              = {
+    Name = "${var.name}-public-subnet1"
+    Environment = var.environment
+  }
+  
+}
